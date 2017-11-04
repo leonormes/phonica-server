2f0d7f15f7f8ae64f84e653eb1b0cd672aa6562b
Leon Ormes <leonormes@gmail.com>
2017-11-04 14:28:59 +0000

    Refactor phoneme model

--- server/models/phoneme.js
+++ server/models/phoneme.js
@@ -1,15 +1,23 @@
-module.exports = (sequelize, DataTypes) => {
-  const Phoneme = sequelize.define('Phoneme', {
-    phoneme: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
-  Phoneme.associate = (models) => {
-    Phoneme.hasMany(models.Grapheme, {
-      foreignKey: 'phonemeId',
-      as: 'grapheme',
-    });
-  };
-  return Phoneme;
-};
+const Sequelize = require('sequelize');
+const db = require('./index');
+const Phoneme = db.define('Phoneme', {
+  phoneme: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    field: 'id',
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: true,
+  },
+  uniCode: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+});
+
+module.exports = Phoneme;
