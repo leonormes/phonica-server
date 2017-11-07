8ff431d8031dbcbaccf083610db4c56fb4453faa
Leon Ormes <leonormes@gmail.com>
2017-11-07 07:59:23 +0000

    Create a reactive version of the seed file to handle creating and associating words

--- seed.js
+++ seed.js
@@ -1,4 +1,5 @@
 const db = require('./db');
+let Rx = require('rxjs/Rx');
 // importing Bluebird promises so we can Promise.map
 const Promise = require('bluebird');
 const Phoneme = require('./db').phonemes;
