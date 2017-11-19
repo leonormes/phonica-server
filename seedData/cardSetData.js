d58c8dd2167560b9b6d7de36efcd09ac6f2d6743
Leon Ormes <leonormes@gmail.com>
2017-11-19 20:12:22 +0000

    update schemes

--- seedData/cardSetData.js
+++ seedData/cardSetData.js
@@ -24,4 +24,74 @@ module.exports = cardSetData = [
     order: 5,
     phonicScheme: 'Jolly Phonics',
   },
+  {
+    name: 'las1',
+    order: 1,
+    phonicScheme: 'Letters and Sounds',
+  },
+  {
+    name: 'las2',
+    order: 2,
+    phonicScheme: 'Letters and Sounds',
+  },
+  {
+    name: 'las3',
+    order: 3,
+    phonicScheme: 'Letters and Sounds',
+  },
+  {
+    name: 'las4',
+    order: 4,
+    phonicScheme: 'Letters and Sounds',
+  },
+  {
+    name: 'las5',
+    order: 5,
+    phonicScheme: 'Letters and Sounds',
+  },
+  {
+    name: 'las6',
+    order: 6,
+    phonicScheme: 'Letters and Sounds',
+  },
+  {
+    name: 'las7',
+    order: 7,
+    phonicScheme: 'Letters and Sounds',
+  },
+  {
+    name: 'las8',
+    order: 8,
+    phonicScheme: 'Letters and Sounds',
+  },
+  {
+    name: 'rwi1',
+    order: 1,
+    phonicScheme: 'Read, Write Inc',
+  },
+  {
+    name: 'rwi2',
+    order: 2,
+    phonicScheme: 'Read, Write Inc',
+  },
+  {
+    name: 'rwi3',
+    order: 3,
+    phonicScheme: 'Read, Write Inc',
+  },
+  {
+    name: 'rwi4',
+    order: 4,
+    phonicScheme: 'Read, Write Inc',
+  },
+  {
+    name: 'rwi5',
+    order: 5,
+    phonicScheme: 'Read, Write Inc',
+  },
+  {
+    name: 'rwi6',
+    order: 6,
+    phonicScheme: 'Read, Write Inc',
+  },
 ];
